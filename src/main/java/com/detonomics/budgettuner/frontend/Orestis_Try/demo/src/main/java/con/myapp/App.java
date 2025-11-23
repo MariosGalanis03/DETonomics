b73ba8cc4ab5e06776878f03ddee331b611051fb
Orestis Pantos <t8240100@aueb.gr>
2025-11-23 14:34:12 +0200

    put image and color

--- src/main/java/com/detonomics/budgettuner/frontend/Orestis_Try/demo/src/main/java/con/myapp/App.java
+++ src/main/java/com/detonomics/budgettuner/frontend/Orestis_Try/demo/src/main/java/con/myapp/App.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.util.Objects;
 
 import java.io.IOException;
 
@@ -18,7 +20,15 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
+        /*code for the logo to appear in top bar and botom taskbar */
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("logo.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.out.println("Η εικόνα δεν βρέθηκε, έλεγξε το όνομα ή τον φάκελο!");
+        }
         stage.show();
     }
 
